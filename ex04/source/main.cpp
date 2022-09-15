682d1ed8de678b3f2f0ba8e063f716ed4565ebd6
Nic Flenghi <nic.flenghi@gmail.com>
2022-09-15 06:23:36 +0000

    ex04 test script

---
+++ ex04/source/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fnichola <fnichola@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/15 01:47:40 by fnichola          #+#    #+#             */
+/*   Updated: 2022/09/15 06:23:15 by fnichola         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+
+void replace(const char* cFilename, const char* cS1, const char* cS2)
+{
+	std::string s1(cS1);
+	std::string s2(cS2);
+	std::string filename(cFilename);
+	std::string line;
+	
+	std::ifstream inFile(filename.c_str());
+	if (!inFile.good())
+	{
+		std::perror("Error: Can't open input file");
+		return ;
+	}
+	std::ofstream outFile(filename.append(".replace").c_str());
+	if (!outFile.good())
+	{
+		std::perror("Error: Can't open output file");
+		return ;
+	}
+	if (s1.length() == 0)
+	{
+		std::cerr << "Error: First argument cannot be blank" << std::endl;
+		return ;
+	}
+	while(std::getline(inFile, line))
+	{
+		size_t found;
+
+		found = line.find(s1);
+		while (found != std::string::npos) 
+		{
+			line.erase(found, s1.length());
+			line.insert(found, s2);
+			found = line.find(s1);
+		}
+		outFile << line;
+		if (inFile.good())
+			outFile << std::endl;
+	}
+	inFile.close();
+	outFile.close();
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 4)
+		replace(argv[1], argv[2], argv[3]);
+	else
+		std::cerr << "Must run with three arguments: <filename> <s1> <s2>" << std::endl;
+	return (0);
+}
---
+++ ex04/test.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+NAME=./a.out
+
+print_test_header()
+{
+	echo "---------------"
+	echo Test \#$1
+	echo $2
+	echo "---------------"
+}
+
+print_test_header 1 "Simple replacement"
+(
+	set -x
+	$NAME test1 twinkle hello
+	cat -e test1.replace
+	diff --color test1 test1.replace
+)
+echo ""
+
+print_test_header 2 "Word to replace doesn't exist"
+(
+	set -x
+	$NAME test2 asdf hello
+	#cat -e test2.replace
+	diff --color test2 test2.replace
+)
+echo ""
+
+print_test_header 3 "Source file doesn't end with newline"
+(
+	set -x
+	$NAME test3 little big
+	cat -e test3.replace
+)
+echo ""
+
+print_test_header 4 "Can't open input file"
+(
+	set -x
+	$NAME test4 twinkle hello
+	cat -e test4.replace
+)
+echo ""
+
+print_test_header 5 "Can't open output file"
+(
+	set -x
+	$NAME test5 twinkle hello
+	cat -e test5.replace
+)
+echo ""
+
+print_test_header 6 "Blank first arg"
+(
+	set -x
+	$NAME test6 "" hello
+	#cat -e test6.replace
+	diff --color test6 test6.replace
+)
+echo ""
+
+print_test_header 7 "Blank second arg"
+(
+	set -x
+	$NAME test7 twinkle ""
+	#cat -e test7.replace
+	diff --color test7 test7.replace
+)
+echo ""
+
+print_test_header 8 "Too many arguments"
+(
+	set -x
+	$NAME test8 twinkle hello another
+	#cat -e test8.replace
+	diff --color test8 test8.replace
+)
+echo ""
+
+print_test_header 9 "Not enough arguments"
+(
+	set -x
+	$NAME test9 twinkle
+	#cat -e test8.replace
+	diff --color test9 test9.replace
+)
+echo ""
